35e9aeb204acba5452c0f7c4ad1c5b707fb6116f
hendrikwiersma <hendrikwiersma@gmail.com>
2015-05-23 22:13:19 +0300

    New highway scenario. Fixed the skybox changer. Put mirrors in the red car.

--- StoplichtProjectClient/Assets/Environment/SkyboxController.cs
+++ StoplichtProjectClient/Assets/Environment/SkyboxController.cs
@@ -15,6 +15,7 @@ public class SkyboxController : MonoBehaviour {
 	public GameObject CubeSkybox5;
 	Color fullDark = new Color(32.0f / 255.0f, 28.0f / 255.0f, 46.0f / 255.0f);
     Color fullLight = new Color(253.0f / 255.0f, 248.0f / 255.0f, 223.0f / 255.0f);
+    public GameObject User;
 	// Use this for initialization
 	void Start () {
 	
@@ -26,26 +27,41 @@ public class SkyboxController : MonoBehaviour {
 			RenderSettings.skybox = Skybox1;
 			Destroy(SkyboxCube);
 			SkyboxCube = Instantiate(CubeSkybox1, new Vector3(0,0,0), Quaternion.Euler(-90,0,0)) as GameObject;
+			SlowRotate rotatescript = SkyboxCube.GetComponent<SlowRotate>();
+			rotatescript.User = User;
+			SkyboxCube.transform.parent = transform;
 		}
 		else if (Input.GetKeyDown(KeyCode.F2)){
 			RenderSettings.skybox = Skybox2;
 			Destroy(SkyboxCube);
 			SkyboxCube = Instantiate(CubeSkybox2, new Vector3(0,0,0), Quaternion.Euler(-90,0,0)) as GameObject;
+			SlowRotate rotatescript = SkyboxCube.GetComponent<SlowRotate>();
+			rotatescript.User = User;
+			SkyboxCube.transform.parent = transform;
 		}
 		else if (Input.GetKeyDown(KeyCode.F3)){
 			RenderSettings.skybox = Skybox3;
 			Destroy(SkyboxCube);
 			SkyboxCube = Instantiate(CubeSkybox3, new Vector3(0,0,0), Quaternion.Euler(-90,0,0)) as GameObject;
-			}
+			SlowRotate rotatescript = SkyboxCube.GetComponent<SlowRotate>();
+			rotatescript.User = User;
+			SkyboxCube.transform.parent = transform;
+		}
 		else if (Input.GetKeyDown(KeyCode.F4)){
 			RenderSettings.skybox = Skybox4;
 			Destroy(SkyboxCube);
 			SkyboxCube = Instantiate(CubeSkybox4, new Vector3(0,0,0), Quaternion.Euler(-90,0,0)) as GameObject;
+			SlowRotate rotatescript = SkyboxCube.GetComponent<SlowRotate>();
+			rotatescript.User = User;
+			SkyboxCube.transform.parent = transform;
 		}
 		else if (Input.GetKeyDown(KeyCode.F5)){
 			RenderSettings.skybox = Skybox5;
 			Destroy(SkyboxCube);
 			SkyboxCube = Instantiate(CubeSkybox5, new Vector3(0,0,0), Quaternion.Euler(-90,0,0)) as GameObject;
+			SlowRotate rotatescript = SkyboxCube.GetComponent<SlowRotate>();
+			rotatescript.User = User;
+			SkyboxCube.transform.parent = transform;
 		}
 	}
 }
