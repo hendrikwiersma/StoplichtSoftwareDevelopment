adacb6e87a6b9b509f168caa4824a898a4f1b8ae
hendrikwiersma <hendrikwiersma@gmail.com>
2015-05-24 00:10:42 +0300

    Added a parking challange.

--- StoplichtProjectClient/Assets/Vehicle/SlowRotate.cs
+++ StoplichtProjectClient/Assets/Vehicle/SlowRotate.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class SlowRotate : MonoBehaviour {
 	public GameObject User;
+	public float speed = 0.05f;
+	public bool useUser = true;
 
 	// Use this for initialization
 	void Start () {
@@ -11,7 +13,9 @@ public class SlowRotate : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Rotate(0, 0, 0.5f*Time.deltaTime);
-		transform.position = User.transform.position;
+		transform.Rotate(0, 0, speed*Time.deltaTime);
+		if(useUser){
+			transform.position = User.transform.position;
+		}
 	}
 }
---
+++ StoplichtProjectClient/Assets/parking_space_controller.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class parking_space_controller : MonoBehaviour {
+	private int triggerCounter = 0;
+	public int numberOfTriggers = 2;
+	public void addTrigger(){
+		triggerCounter++;
+		if(triggerCounter >= numberOfTriggers){
+			AudioSource audio = GetComponent<AudioSource>();
+			audio.Play();
+		}
+	}
+	public void removeTrigger(){
+		triggerCounter--;
+	}
+}
