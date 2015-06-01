c78392e3e4ee375bc228f5135f9fd7108f9e52d1
Bauke <bauke_jansen@hotmail.com>
2015-06-01 15:09:22 +0200

    - pedestrian trafficlights

---
+++ StoplichtProjectClient/Assets/TrafficlighPedestrian.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrafficlighPedestrian : Trafficlight {
+		
+		void Start () {
+			
+			base._Start ();
+			
+		}
+		
+		// Update intersection with trafficlight state
+		protected override void SetState() {
+			
+			Intersection intersection = transform.parent.GetComponent<Intersection>();
+			intersection.SetLightState(direction.ToString(), State.ToString());
+			
+		}
+		
+	}
